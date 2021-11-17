d62ed2dd7fd3d09966d545761433820e7058b0bf
Whizzie99 <47413415+Whizzie99@users.noreply.github.com>
2021-11-17 12:58:42 +0100

    install formspree dependency

--- components/ContactForm/ContactForm.jsx
+++ components/ContactForm/ContactForm.jsx
@@ -1,13 +1,22 @@
+import { useForm, ValidationError } from "@formspree/react";
 import { StyledContactForm } from "./styles";
 
 const ContactForm = () => {
+
+    const [state, handleSubmit] = useForm("contactUs")
+
+    if(state.succeeded){
+        <h3>Thank you!</h3>
+    }
+
     return (
         <StyledContactForm>
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <input type="text" placeholder="Full name" />
-                <input type="email" placeholder="email" />
+                <input type="email" field="email" prefix="Email" placeholder="email" />
+                <ValidationError errors={state.errors}/>
                 <textarea name="" id="" cols="30" rows="5" placeholder="message"></textarea>
-                <button>submit</button>
+                <button type="submit" disabled={state.submitting}>submit</button>
             </form>
         </StyledContactForm>
     );
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
+import { FormspreeProvider } from '@formspree/react'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <FormspreeProvider project="1813524319930678705">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </FormspreeProvider>
   )
 }
 
