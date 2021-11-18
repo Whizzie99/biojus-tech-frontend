bcf91d70269db7903ba8aa10c951d12d7f1fe321
Whizzie99 <47413415+Whizzie99@users.noreply.github.com>
2021-11-18 08:43:26 +0100

    integrate nodemailer for contact form

--- components/ContactForm/ContactForm.jsx
+++ components/ContactForm/ContactForm.jsx
@@ -1,22 +1,53 @@
-import { useForm, ValidationError } from "@formspree/react";
+import { useState } from "react";
 import { StyledContactForm } from "./styles";
 
 const ContactForm = () => {
 
-    const [state, handleSubmit] = useForm("contactUs")
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const [message, setMessage] = useState()
+    const [submitted, setSubmitted] = useState(false)
 
-    if(state.succeeded){
-        <h3>Thank you!</h3>
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        console.log('sending')
+
+        let data = {
+            name, 
+            email,
+            message
+        }
+
+        fetch('/api/contact', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json' 
+            },
+            body: JSON.stringify(data)
+        }).then((res) => {
+            console.log('Response received')
+            if(res.status === 200){
+                console.log('succeeded!')
+                setName('')
+                setEmail('')
+                setMessage('')
+            }
+        })
+
+        setSubmitted(true)
     }
 
     return (
         <StyledContactForm>
-            <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Full name" />
-                <input type="email" field="email" prefix="Email" placeholder="email" />
-                <ValidationError errors={state.errors}/>
-                <textarea name="" id="" cols="30" rows="5" placeholder="message"></textarea>
-                <button type="submit" disabled={state.submitting}>submit</button>
+            <form>
+                <input type="text" placeholder="Full name" onChange={(e) =>{setName(e.target.value)}} value={name}/>
+
+                <input type="email" field="email" prefix="Email" placeholder="email" onChange={(e) => {setEmail(e.target.value)}} value={email}/>
+
+                <textarea name="" id="" cols="30" rows="5" placeholder="message" onChange={(e) =>{setMessage(e.target.value)}} value={message}></textarea>
+
+                <button type="submit" onClick={(e) => handleSubmit(e)}>submit</button>
             </form>
         </StyledContactForm>
     );
